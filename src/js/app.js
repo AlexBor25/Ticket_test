614b0eb44d71e415968588143af61e075929e68c
AlexBor <leo74710@gmail.com>
2020-10-06 21:52:59 +0300

    TicketsApp_step3

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import locations from './store/locations';
+import './plugins';
+import '../css/style.css';
 
-locations.init().then(res =>{
+locations.init().then(res => {
     console.log(res);
     console.log(locations);
     console.log(locations.getCitiesByCountryCode('RU'));
